091413c01402cccd5f033b3a1f145258879d26ae
citeb.eduardo <eduardocaroca@citeb.com.br>
2023-07-20 17:36:21 -0300

    fix: ajuste na nomeclatura

--- src/main/java/com/desafio/setor/SetorController.java
+++ src/main/java/com/desafio/setor/SetorController.java
@@ -10,40 +10,44 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
+@Tag(name = "setor")
+@RequestMapping(value = "/setor")
 public class SetorController {
 	@Autowired
 	private SetorService setorService;
 
-	@PostMapping("/setor")
+	@PostMapping("")
 	@Operation(summary = "Adiciona um setor com um cargo", method = "POST")
 	public Setor adicionarSetor(@Validated @RequestBody Setor setor) {
 		return setorService.adicionarSetor(setor);
 	}
 	
-	@PutMapping("/setor/{id}")
+	@PutMapping("/{id}")
 	@Operation(summary = "Editar um setor", method = "PUT")
 	public Setor editarSetor(@PathVariable Long id, @RequestBody Setor setorAtualizado) {
 		return setorService.editarSetor(id, setorAtualizado);
 	}
 	
-	@DeleteMapping("/setor/{id}")
+	@DeleteMapping("/{id}")
 	@Operation(summary = "Deletar um setor", method = "DELETE")
 	public boolean deletarSetor(@PathVariable Long id) {
 		return setorService.deletarSetor(id);
 	}
 
-	@GetMapping("/setor")
+	@GetMapping("")
 	@Operation(summary = "Listar todos os setores", method = "GET")
 	public List<Setor> listarSetor() {
 		return setorService.listarSetores();
 	}
 	
-	@GetMapping("/setor/{id}")
+	@GetMapping("/{id}")
 	@Operation(summary = "Buscar um setor por ID", method = "GET")
 	public Setor listarSetorById(@PathVariable Long id) {
 		return setorService.listarSetorById(id);
--- src/main/java/com/desafio/trabalhador/TrabalhadorController.java
+++ src/main/java/com/desafio/trabalhador/TrabalhadorController.java
@@ -10,40 +10,44 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
+@Tag(name = "trabalhador")
+@RequestMapping(value = "/trabalhador")
 public class TrabalhadorController {
 	@Autowired
 	private TrabalhadorService trabalhadorService;
 
-	@PostMapping("/trabalhador")
+	@PostMapping("")
 	@Operation(summary = "Adiciona um trabalhador com um cargo e setor", method = "POST")
 	public Trabalhador adicionarCargo(@Validated @RequestBody Trabalhador trabalhador) {
 		return trabalhadorService.adicionarTrabalhador(trabalhador);
 	}
 
-	@PutMapping("/trabalhador/{id}")
+	@PutMapping("/{id}")
 	@Operation(summary = "Editar um trabalhador", method = "PUT")
 	public Trabalhador editarSetor(@PathVariable Long id, @RequestBody Trabalhador trabalhadorAtualizado) {
 		return trabalhadorService.editarTrabalhador(id, trabalhadorAtualizado);
 	}
 
-	@DeleteMapping("/trabalhador/{id}")
+	@DeleteMapping("/{id}")
 	@Operation(summary = "Deletar um trabalhador", method = "DELETE")
 	public boolean deletarTrabalhador(@PathVariable Long id) {
 		return trabalhadorService.deletarTrabalhador(id);
 	}
 
-	@GetMapping("/trabalhador")
+	@GetMapping("")
 	@Operation(summary = "Listar todos os trabalhadores", method = "GET")
 	public List<Trabalhador> listarTrabalhadores() {
 		return trabalhadorService.listarTrabalhadores();
 	}
 
-	@GetMapping("/trabalhador/{id}")
+	@GetMapping("/{id}")
 	@Operation(summary = "Buscar um trabalhador por ID", method = "GET")
 	public Trabalhador listarTrabalhadorById(@PathVariable Long id) {
 		return trabalhadorService.listarTrabalhadorById(id);
